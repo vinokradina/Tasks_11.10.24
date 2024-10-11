4897c160416033d2e79757e3d77fd6a00c59cdf0
vinokradina <106469305+vinokradina@users.noreply.github.com>
2024-10-11 13:47:58 +0300

    Add files via upload

---
+++ Task2/Task2/Task2.cpp
@@ -0,0 +1,50 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    HANDLE hNewBuffer, hStdOutOld;
+    char buffer[256];
+    DWORD dwWritten;
+
+    hNewBuffer = CreateConsoleScreenBuffer(
+        GENERIC_READ | GENERIC_WRITE,
+        0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+
+    if (hNewBuffer == INVALID_HANDLE_VALUE)
+    {
+        cout << "CreateConsoleScreenBuffer failed." << endl;
+        return GetLastError();
+    }
+
+    hStdOutOld = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    if (!SetConsoleActiveScreenBuffer(hNewBuffer))
+    {
+        cout << "SetConsoleActiveScreenBuffer failed." << endl;
+        return GetLastError();
+    }
+
+    ifstream file("example.txt");
+    if (!file)
+    {
+        cout << "Failed to open file." << endl;
+        return 1;
+    }
+
+    while (file.getline(buffer, sizeof(buffer)))
+    {
+        WriteConsole(hNewBuffer, buffer, strlen(buffer), &dwWritten, NULL);
+        WriteConsole(hNewBuffer, "\n", 1, &dwWritten, NULL);
+    }
+
+    file.close();
+
+    SetConsoleActiveScreenBuffer(hStdOutOld);
+
+    CloseHandle(hNewBuffer);
+
+    return 0;
+}
---
+++ Task3/Task3/Task3.cpp
@@ -0,0 +1,38 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+void printFile(const char* fileName, bool suppressErrors)
+{
+    ifstream file(fileName);
+    if (!file)
+    {
+        if (!suppressErrors)
+        {
+            cerr << "Error: File " << fileName << " not found." << endl;
+        }
+        return;
+    }
+
+    char buffer[256];
+    while (file.getline(buffer, sizeof(buffer)))
+    {
+        cout << buffer << endl;
+    }
+    file.close();
+}
+
+int main()
+{
+    bool suppressErrors = false;
+    vector<string> fileNames = { "file1.txt", "file2.txt", "file3.txt" };
+
+    for (const auto& fileName : fileNames)
+    {
+        printFile(fileName.c_str(), suppressErrors);
+    }
+
+    return 0;
+}
